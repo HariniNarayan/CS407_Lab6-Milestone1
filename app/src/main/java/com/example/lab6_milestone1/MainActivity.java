46ba7523642bd8faa13b443443b0178052436309
harininarayan <harini.s.narayan@gmail.com>
2021-11-06 14:37:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab6_milestone1/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lab6_milestone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity {
+
+    //Somewhere in Australia
+    private final LatLng mDestinationLatLng = new LatLng(43.0750779, -89.3964526);
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions()
+                        .position(mDestinationLatLng)
+                        .title("Destination"));
+            
+        });
+    }
+}
